421ec44f895bf0e58b70c57f4287320467318961
a-naraikin <anna.naraikin@yandex.ru>
2021-10-04 00:10:09 +0300

    fix: class GameSavingLoader

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -3,8 +3,6 @@ import read from './reader';
 
 export default class GameSavingLoader {
   static load() {
-    return new Promise((resolve) => {
-      resolve(read().then((data) => json(data)));
-    });
+    return read().then((data) => json(data));
   }
 }
